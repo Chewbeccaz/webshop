3409b3c7d966a0d0453657de739e27c204fa5f43
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-04-23 16:39:51 +0200

    added adminpage

--- server/server.js
+++ server/server.js
@@ -89,11 +89,11 @@ app.post("/create-product", async (request, response) => {
       .then(console.log("created product"));
 
     const product = new Product({
-      name: "Grapes",
-      description: "Grapes",
-      price: 39,
+      name: "Watermelon",
+      description: "watermelon sugar..",
+      price: 79,
       image:
-        "https://images.pexels.com/photos/708777/pexels-photo-708777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.unsplash.com/photo-1595475207225-428b62bda831?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       inStock: 30,
       status: "active",
     });
